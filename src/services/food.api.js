5ac8c3e6854fd7e015aad771648891c97b698c62
AditiRajPurohit15 <aditipurohit1505@gmail.com>
2026-03-16 15:32:50 +0530

    get food for partner api frontend

--- src/services/food.api.js
+++ src/services/food.api.js
@@ -4,11 +4,17 @@ export const getFoodItems = async()=>{
     const response = await api.get("/api/food");
     return response.data;
 }
+
 export const createFood = async(data)=>{
     const response = await api.post("/api/food",data,{
         headers:{
             "Content-Type":"multipart/form-data"
         }
     });
+    return response.data;
+}
+
+export const getFoodForPartner = async()=>{
+    const response = await api.get("/api/food/getFood");
     return response.data;
 }
