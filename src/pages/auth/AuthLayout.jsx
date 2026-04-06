87126730d3cb3a23d6fe5d20e9497fd20b203065
AditiRajPurohit15 <aditipurohit1505@gmail.com>
2026-04-06 14:41:17 +0530

    cat animation

--- src/pages/auth/AuthLayout.jsx
+++ src/pages/auth/AuthLayout.jsx
@@ -1,14 +1,33 @@
+import bgVideo from "../../assets/cat.mp4"; // adjust path
 const AuthLayout = ({ children }) => {
 
   return (
     <div className="min-h-screen flex">
 
       {/* LEFT SIDE BRAND */}
-      <div className="hidden md:flex w-1/2 bg-gradient-to-br from-yellow-400 to-orange-500 items-center justify-center">
-        <h1 className="text-white text-5xl font-bold">
-          ReelBite 🍔
-        </h1>
-      </div>
+      <div className="hidden md:block w-1/2 relative">
+
+  {/* VIDEO */}
+  <video
+    src={bgVideo}
+    autoPlay
+    loop
+    muted
+    playsInline
+    className="absolute inset-0 w-full h-full object-cover"
+  />
+
+  {/* DARK OVERLAY */}
+  <div className="absolute inset-0 bg-black/40" />
+
+  {/* TEXT OVER VIDEO
+  <div className="absolute inset-0 flex items-center justify-center">
+    <h1 className="text-white text-5xl font-bold">
+      ReelBite 🍔
+    </h1>
+  </div> */}
+
+</div>
 
       {/* RIGHT SIDE CONTENT */}
       <div className="flex w-full md:w-1/2 items-center justify-center bg-gray-100">
