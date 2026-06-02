6af4451f54a16aca9d359649cb14afc048e46b32
AditiRajPurohit15 <aditipurohit1505@gmail.com>
2026-06-02 14:30:49 +0530

    feat: add comment modal, comment APIs and comment rendering

---
+++ src/components/CommentModal.jsx
@@ -0,0 +1,47 @@
+import React from 'react';
+import { useEffect, useState } from "react";
+import { getComments } from '../services/comment.api';
+
+const CommentModal = ({foodId, onClose}) => {
+    console.log(foodId)
+    const [comments, setComments] = useState([]);
+    
+    useEffect(()=>{
+        const fetchComments = async ()=> {
+        try {
+            const data = await getComments(foodId);
+            setComments(data.comments);
+            console.log(data.comments);
+        } catch (error) {
+             console.log("error fetching comments");
+        }
+    }
+    fetchComments();
+    },[foodId])
+  return (
+  <div className="fixed inset-0 bg-white z-[9999] text-black p-4">
+
+    <div className="flex justify-between items-center mb-4">
+      <h1 className="text-xl font-semibold">Comments</h1>
+
+      <button
+        onClick={onClose}
+        className="text-2xl font-bold"
+      >
+        ✕
+      </button>
+    </div>
+
+    <p>Total comments: {comments.length}</p>
+
+    {comments.map((comment) => (
+      <div key={comment._id}>
+        <p>{comment.user.fullName}</p>
+        <p>{comment.text}</p>
+      </div>
+    ))}
+  </div>
+);
+}
+
+export default CommentModal;
